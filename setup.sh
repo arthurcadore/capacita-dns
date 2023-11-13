64fff9defcfc566468f02f535d7b720afe648046
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-11-13 00:36:29 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 
 # Script to stop default system resolver, to free 53 port: 
 
-apt-get update && apt-get upgrade -y 
+chmod +x installDocker.sh
 
+./installDocker.sh 
+
+echo "Trying to free UDP/53 Port:"
+sudo systemctl stop systemd-resolved
 sudo systemctl disable systemd-resolved
